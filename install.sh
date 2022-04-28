b5e728c8e96931dc440c9e2903b16e5f3078e0d2
Isaac Lyons <isaac@snowskeleton.net>
2022-04-28 16:55:57 -0400

    Added install file

---
+++ install.sh
@@ -0,0 +1,14 @@
+
+DEST="/usr/local/bin/rsrssb"
+LIB="/usr/lib/rsrssb"
+
+#may need to create directory first. if so, probably also have to add it to PATH
+#sudo mkdir ${LIB}
+
+#uncomment to remove previous installation
+#sudo rm -rf ${LIB} ${DEST}
+
+#comment out to not install (acts as a complete uninstall if paired with the above)
+sudo cp -r ../rsrssb ${LIB}
+sudo ln -s ${LIB}/main.py ${DEST}
+sudo chmod 755 ${DEST}
