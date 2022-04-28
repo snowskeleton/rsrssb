8cf05bfd89681b655c71eef8e9d5de064a024500
Isaac Lyons <isaac@snowskeleton.net>
2022-04-28 17:19:11 -0400

    Restructured

--- src/itemFormatter.py
+++ src/itemFormatter.py
@@ -7,9 +7,9 @@ import re
 def hydrateItem(item):
     newItem = ITEM_TEMPLATE
     newItem = re.sub(';EP_TITLE', item['title'], newItem)
-    newItem = re.sub(';EP_LINK', item['link'], newItem)
+    newItem = re.sub(';EP_LINK', ''.join(item['link']), newItem)
     newItem = re.sub(';EP_DESCRIPTION', item['description'], newItem)
-    newItem = re.sub(';EP_ENCLOSURE_URL', item['enclosureURL'], newItem)
+    newItem = re.sub(';EP_ENCLOSURE_URL', ''.join(item['enclosureURL']), newItem)
     newItem = re.sub(';EP_LENGTH_IN_BYTES', item['enclosureBytes'], newItem)
     newItem = re.sub(';EP_PUBDATE', item['pubdate'], newItem)
     return newItem
