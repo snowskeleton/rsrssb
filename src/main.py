3ce08ceeab07057165e7a4a36c9c8c8801b4badb
Isaac Lyons <isaac@snowskeleton.net>
2022-04-29 13:10:50 -0400

    Converted to using stdin and stdout

--- src/main.py
+++ src/main.py
@@ -16,28 +16,20 @@ def out(string):
 	print(string, file = sys.stdout)
 
 def main():
-	if len(sys.argv) < 2:
-		print("Please supply audio files as arguments")
-		raise SystemExit
+	# if len(sys.argv) < 2:
+	# 	print("Please supply audio files as arguments")
+	# 	raise SystemExit
 
-	for arg in sys.stdin:
-		if audioFileCheck(arg) != False: # i.e., if the argument is a non-audio file
-			ARTWORK = arg
-			sys.argv.pop(arg)
+		# if audioFileCheck(arg) != False: # i.e., if the argument is a non-audio file
+			# ARTWORK = arg
+			# sys.argv.pop(arg)
 
-	# with open('feed.xml', 'w') as output:
 	#add header to feed.xml. only need to do this once per file
 	bt = removeVars(HEADER)
 	out(bt)
 
-  #remove script name from arguments, leaving just audio file names
-	sys.argv.pop(0)
-
-
-	# gotta make sure we only make listings for real audio files
-
 	#work through all the audio files, making a listing for each.
-	for i in sys.argv:
+	for i in sys.stdin:
 		if audioFileCheck(i) == True:
 			#convert string into dict
 			som = itemToDict(i)
