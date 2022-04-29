f632cf187fda00b59f6e8d15f6cb551b41e2da01
Isaac Lyons <isaac@snowskeleton.net>
2022-04-29 13:25:30 -0400

    Changed back from stdin, because otherwise I can't get the filesize

--- src/main.py
+++ src/main.py
@@ -12,35 +12,40 @@ def audioFileCheck(item):
 		return False
 	return True
 
-def out(string):
-	print(string, file = sys.stdout)
-
 def main():
-	# if len(sys.argv) < 2:
-	# 	print("Please supply audio files as arguments")
-	# 	raise SystemExit
+	if len(sys.argv) < 2:
+		print("Please supply audio files as arguments")
+		raise SystemExit
 
-		# if audioFileCheck(arg) != False: # i.e., if the argument is a non-audio file
-			# ARTWORK = arg
+	for arg in sys.argv:
+		if audioFileCheck(arg) != False: # i.e., if the argument is a non-audio file
+			ARTWORK = arg
 			# sys.argv.pop(arg)
 
-	#add header to feed.xml. only need to do this once per file
-	bt = removeVars(HEADER)
-	out(bt)
+	with open('feed.xml', 'w') as output:
+		#add header to feed.xml. only need to do this once per file
+		bt = removeVars(HEADER)
+		output.write(bt)
 
-	#work through all the audio files, making a listing for each.
-	for i in sys.stdin:
-		if audioFileCheck(i) == True:
-			#convert string into dict
-			som = itemToDict(i)
+  	#remove script name from arguments, leaving just audio file names
+		sys.argv.pop(0)
 
-			#exchange dict for loooooong string
-			som = hydrateItem(som)
 
-			#write long string to file
-			out(som)
+		# gotta make sure we only make listings for real audio files
 
-	out(FOOTER)
+		#work through all the audio files, making a listing for each.
+		for i in sys.argv:
+			if audioFileCheck(i) == True:
+				#convert string into dict
+				som = itemToDict(i)
+
+				#exchange dict for loooooong string
+				som = hydrateItem(som)
+
+				#write long string to file
+				output.write(som)
+
+		output.write(FOOTER)
 
 if __name__ == '__main__':
 	main()
