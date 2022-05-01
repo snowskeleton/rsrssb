0a91c4a8fce7ff4317fc98378f1cda314411a3fd
Isaac Lyons <isaac@snowskeleton.net>
2022-05-01 00:24:16 -0400

    Sort the items before returning them as a list

--- src/feed.py
+++ src/feed.py
@@ -2,6 +2,8 @@ import os
 import re
 from datetime import datetime, timedelta
 
+from numpy import sort
+
 from template import ITEM_TEMPLATE, FOOTER
 from templateEditor import header
 from input import Input
@@ -19,22 +21,34 @@ class Feed():
 
   def xml(self):
     feed = header(self)
-    for item in Item.instances:
+    for item in Item.instances():
       feed += item.spaghetti()
     feed += FOOTER
     return feed
 
 
 class Item():
-  instances = []
+  _instances = []
+  @classmethod
+  def instances(self):
+    return sort(self._instances)
+
+  # the following two functions are defined so that proper sorting
+  # can be done on Item objects
+  def __lt__(self, other):
+    return self.title < other.title
+  def __gt__(self, other):
+    return self.title > other.title
+
   def __init__(self, fileName, parent) -> None:
       self.title = re.sub(r'.mp3|.m4b|.mb3|.mb4', '', fileName)
       self.ep_link = f'{parent.link}/rss/{parent.linkTitle}/{fileName}'
       self.enclosureURL = f'{parent.link}/rss/{parent.linkTitle}/{fileName}'
       self.description = fileName
       self.bytes = f'{os.path.getsize(fileName)}'
-      self.pubdate = f'{(datetime.now() + timedelta(days=len(self.instances)))}'
-      self.__class__.instances.append(self)
+      self.pubdate = f'{(datetime.now() + timedelta(days=len(self.instances())))}'
+      self.__class__._instances.append(self)
+
 
   def spaghetti(self) -> str:
     newItem = ITEM_TEMPLATE
--- testFeed.sh
+++ testFeed.sh
@@ -1,3 +1,3 @@
 touch test_chap1.mp3 test_chap2.mp3 test_chap3.mp3
-python3 main.py --title "this is really long" --domain snowskeleton.net
+python3 src/main.py --title "this is really long" --domain snowskeleton.net
 rm test_chap1.mp3 test_chap2.mp3 test_chap3.mp3
