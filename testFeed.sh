7e1954abec9b67d45704a67df7b39314120683af
Isaac Lyons <isaac@snowskeleton.net>
2022-05-01 02:00:41 -0400

    Added spaces to the file names for increased testing coverage

--- testFeed.sh
+++ testFeed.sh
@@ -1,30 +1,30 @@
 mkdir .tmp
 cd .tmp
 touch \
-test_chap01.mp3 \
-test_chap02.mp3 \
-test_chap03.mp3 \
-test_chap04.mp3 \
-test_chap05.mp3 \
-test_chap06.mp3 \
-test_chap07.mp3 \
-test_chap08.mp3 \
-test_chap09.mp3 \
-test_chap10.mp3 \
-test_chap11.mp3
+'test_chap 01.mp3' \
+'test_chap 02.mp3' \
+'test_chap 03.mp3' \
+'test_chap 04.mp3' \
+'test_chap 05.mp3' \
+'test_chap 06.mp3' \
+'test_chap 07.mp3' \
+'test_chap 08.mp3' \
+'test_chap 09.mp3' \
+'test_chap 10.mp3' \
+'test_chap 11.mp3'
 
 touch \
-test_chap01.m4b \
-test_chap02.m4b \
-test_chap03.m4b \
-test_chap04.m4b \
-test_chap05.m4b \
-test_chap06.m4b \
-test_chap07.m4b \
-test_chap08.m4b \
-test_chap09.m4b \
-test_chap10.m4b \
-test_chap11.m4b
+'test_chap 01.m4b' \
+'test_chap 02.m4b' \
+'test_chap 03.m4b' \
+'test_chap 04.m4b' \
+'test_chap 05.m4b' \
+'test_chap 06.m4b' \
+'test_chap 07.m4b' \
+'test_chap 08.m4b' \
+'test_chap 09.m4b' \
+'test_chap 10.m4b' \
+'test_chap 11.m4b'
 
 python3 ../src/main.py --title "this is really long" --domain snowskeleton.net
 mv feed.xml ../ && rm -rf ../.tmp
