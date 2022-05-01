75680fc94c2323e22add46f8e9817f3968025cb5
Isaac Lyons <isaac@snowskeleton.net>
2022-05-01 06:41:38 -0400

    Formatting

--- src/input.py
+++ src/input.py
@@ -2,8 +2,22 @@ import argparse
 
 
 parser = argparse.ArgumentParser(description='Argument parser description')
-parser.add_argument('--domain', dest='domain', type=str, action='store', help='Base URL (e.g., example.net)', default='')
-parser.add_argument('--title', dest='title', type=str, action='store', help='The name of your feed', default='')
+parser.add_argument(
+  '--domain',
+  dest='domain',
+  type=str,
+  action='store',
+  help='Base URL (e.g., example.net)',
+  default=''
+  )
+parser.add_argument(
+  '--title',
+  dest='title',
+  type=str,
+  action='store',
+  help='The name of your feed',
+  default=''
+  )
 args = parser.parse_args()
 
 class Input():
