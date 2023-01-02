1887aa242db08498921882a7e1dcebe379574e01
snowskeleton <isaac@snowskeleton.net>
2023-01-02 19:06:37 +0000

    error handling

--- src/rsrssb/episode.py
+++ src/rsrssb/episode.py
@@ -25,21 +25,23 @@ class Episode():
             des = tags.extra['description']
             des = des.encode('raw_unicode_escape')
             des = des.decode('unicode_escape')
-            des = des if des != None else self.title
+            des = des if des is not None else self.title
             self.description = des
         else:
-            self.description = tags.comment
+            des = tags.comment if tags.comment is not None else self.title
+            self.description = des
+
         t = datetime.now().replace(year=int(tags.year))
         t -= timedelta(days=len(self.instances))
         self.pubDate = format_datetime(t)
-        self.__class__.instances.append(self)
 
         # file size in bytes (required for podcast feed)
         if os.path.exists(self.filename):
             self.bytes = f'{os.path.getsize(self.filename)}'
         else:
-            print(f'Unable to locate file: {self.filename}')
-            self.bytes = '0'
+            raise (FileNotFoundError(self.filename))
+
+        self.__class__.instances.append(self)
 
     # the following two functions proper sorting to be done on Episode objects
     def __lt__(self, other):
