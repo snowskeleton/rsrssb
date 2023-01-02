bed33bce5fefd34b820e1945fb52627a46d2812d
snowskeleton <isaac@snowskeleton.net>
2023-01-02 04:29:46 +0000

    organize in order of size

--- src/rsrssb/feed.py
+++ src/rsrssb/feed.py
@@ -7,12 +7,12 @@ from .myparser import args
 
 class Feed():
     def __init__(self):
-        self.title = args.title
-        self.root = os.path.abspath(self.title)
-        self.description = self.title
-        self.domain = args.domain
-        self.webmaster = 'webmaster@' + self.domain
-        self.date = f"{datetime.now().strftime('%Y-%m-%d')}"
+        self.date: str = f"{datetime.now().strftime('%Y-%m-%d')}"
+        self.root: str = os.path.abspath(self.title)
+        self.title: str = args.title
+        self.domain: str = args.domain
+        self.webmaster: str = 'webmaster@' + self.domain
+        self.description: str = self.title
 
         # a magic regex that gets the parent dir without any preceding path
         dir = re.sub(r'^.*/', '', os.getcwd())
