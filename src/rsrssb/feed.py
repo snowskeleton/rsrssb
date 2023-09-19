df8b29e457bfa15173fee36a362f90f583196cc7
Isaac Lyons <isaac@snowskeleton.net>
2023-09-19 13:07:24 -0400

    don't derive directory name from where the script is run

--- src/rsrssb/feed.py
+++ src/rsrssb/feed.py
@@ -7,9 +7,8 @@ class Feed():
         self.title: str = title
         self.root: str = os.path.abspath(self.title)
         self.domain: str = domain
-        self.webmaster: str = 'webmaster@' + self.domain
+        self.webmaster: str = 'webmaster@' + re.sub(r'/.*$', '', self.domain)
         self.description: str = self.title
 
         # a magic regex that gets the parent dir without any preceding path
-        dir = re.sub(r'^.*/', '', os.getcwd())
-        self.link = f'https://{self.domain}/{dir}'
+        self.link = f'https://{self.domain}'
